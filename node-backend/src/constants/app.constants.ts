5062417b37862c13c2d9135bdb345d6fec72330a
it21109126 <it21109126@my.sliit.lk>
2024-05-03 04:16:00 +0530

    created prisma schema from sql db schema

--- node-backend/src/constants/app.constants.ts
+++ node-backend/src/constants/app.constants.ts
@@ -10,4 +10,24 @@ const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY!;
 
 const JWT_SECRET: string = process.env.JWT_SECRET!;
 
-export { PORT, MONGO_URI, JWT_SECRET, OPENAI_API_KEY };
+const AZURE_SQL_HOST: string = process.env.AZURE_SQL_HOST!;
+
+const AZURE_SQL_DATABASE: string = process.env.AZURE_SQL_DATABASE!;
+
+const AZURE_SQL_USERNAME: string = process.env.AZURE_SQL_USERNAME!;
+
+const AZURE_SQL_PASSWORD: string = process.env.AZURE_SQL_PASSWORD!;
+
+const AZURE_SQL_PORT: number = parseInt(process.env.AZURE_SQL_PORT!, 1433);
+
+export {
+  PORT,
+  MONGO_URI,
+  JWT_SECRET,
+  OPENAI_API_KEY,
+  AZURE_SQL_HOST,
+  AZURE_SQL_DATABASE,
+  AZURE_SQL_USERNAME,
+  AZURE_SQL_PASSWORD,
+  AZURE_SQL_PORT,
+};
