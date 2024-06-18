22545b384e9683112aee88a7bb133dfb1870bfb4
it21012488 <it21012488@my.sliit.lk>
2024-06-18 12:57:01 +0530

    completed sidebar with static data

--- frontend/src/Layouts/Main.tsx
+++ frontend/src/Layouts/Main.tsx
@@ -1,11 +1,19 @@
 import { PropsWithChildren } from "react";
 import Header from "../Components/Header";
+import Sidebar from "../Components/Sidebar";
 const MainLayout = (props: PropsWithChildren) => {
-  return (
-    <div>
-      <Header />
+  const moduleData = {
+    componentName: "Introduction to Database Management Systems",
+    moduleCode: "SE3020",
+  };
 
-      {props.children}
+  return (
+    <div className="font-roboto w-screen h-screen">
+      <Header />
+      <div className="grid grid-cols-[auto,1fr]">
+        <Sidebar {...moduleData} />
+        {props.children}
+      </div>
     </div>
   );
 };
