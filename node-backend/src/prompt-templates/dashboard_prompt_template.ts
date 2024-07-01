944db9dd1220b28a19e4ec56a1a3e6da2fc1101f
it21109126 <it21109126@my.sliit.lk>
2024-07-01 23:59:51 +0530

    chore: Refactor SQL prompt template and dashboard service

--- node-backend/src/prompt-templates/dashboard_prompt_template.ts
+++ node-backend/src/prompt-templates/dashboard_prompt_template.ts
@@ -8,25 +8,27 @@ export const SQL_MSSQL_PROMPT: PromptTemplate<
     schema: any;
     formatInstructions: any;
     question: any;
+    defaultQuery: string;
   },
   any
 > = new PromptTemplate({
   template: `
-  ===============================================================================================================
-  You are an MS SQL expert. Given an input question, first create a syntactically correct MS SQL query to run, 
-  then look at the results of the query and return the answer to the input question. Unless the user specifies
-  in the question a specific number of results to obtain, query for all the results. Never query for all columns 
-  from a table. You must query only the columns that are needed to answer the question. Pay attention to use only 
-  the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, 
-  pay attention to which column is in which table.
-  
+  You are an MS SQL expert. Given an input question, construct a syntactically correct MS SQL query for it. 
+  Unless the user specifies in the question a specific number of results to obtain, query for all the results. 
+  You must include only the columns that are needed to answer the question. Pay attention to only use the column
+  names you can see in the tables. Also, pay attention to which column is in which table. 
+
   Only use the following database schema:
   {schema}
 
+  If you are not able to construct the SQL query with the available tables and columns, never include any imaginary
+  tables or columns of your choice. Instead, simply return the below query as it is:
+  {defaultQuery}
+
   Please provide the response according to below format:
   {formatInstructions}
-  ===============================================================================================================
+
   Question: {question}
   `,
-  inputVariables: ["schema", "formatInstructions", "question"],
+  inputVariables: ["schema", "formatInstructions", "question", "defaultQuery"],
 });
