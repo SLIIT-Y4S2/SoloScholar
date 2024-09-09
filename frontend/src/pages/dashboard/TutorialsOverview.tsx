e075df9441b21434a3f85aab30e0d972bce3ffed
it21109126 <it21109126@my.sliit.lk>
2024-09-09 21:14:20 +0530

    Implemented two more indicators for tutorial

--- frontend/src/pages/dashboard/TutorialsOverview.tsx
+++ frontend/src/pages/dashboard/TutorialsOverview.tsx
@@ -1,10 +1,16 @@
 import BreadCrumb from "../../Components/BreadCrumb";
 import { Fragment } from "react/jsx-runtime";
-import { Result, Select, Typography } from "antd";
+import { Card, Col, Result, Row, Select, Statistic, Typography } from "antd";
 import { useContext, useEffect, useState } from "react";
 import CustomStackedBarChart from "../../Components/dashboard/CustomStackedBarChart";
 import { DashboardAnalyticsContext } from "../../provider/DashboardAnalyticsContext";
 import { ResultStatusType } from "antd/es/result";
+import CustomLabeledPieChart from "../../Components/dashboard/CustomLabeledPieChart";
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  BookOutlined,
+} from "@ant-design/icons";
 
 const { Text } = Typography;
 
@@ -38,6 +44,8 @@ const TutorialsOverview = () => {
     getLessonsOfModule,
     getTutorialAnalytics,
     academicPerformanceAndLearningStrategiesTutorial,
+    affectiveStateTutorial,
+    summaryStatisticsTutorial,
     customMessage,
   } = useContext(DashboardAnalyticsContext);
 
@@ -111,20 +119,110 @@ const TutorialsOverview = () => {
             />
           </div>
         </div>
-
+        <br />
         <Text>
+          <b>Summary Statistics</b>
+        </Text>
+        <Card>
+          {customMessage ? (
+            <Result
+              status={customMessage.type as ResultStatusType}
+              title={customMessage.content}
+            />
+          ) : (
+            <Row gutter={16} className="flex justify-center">
+              <Col span={8}>
+                <Card bordered={true}>
+                  <Statistic
+                    title="Total Tutorials Generated"
+                    value={summaryStatisticsTutorial?.totalTutorialCount}
+                    prefix={<BookOutlined />}
+                  />
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card bordered={true}>
+                  <Statistic
+                    title="Average Tutorial Completion Rate"
+                    value={summaryStatisticsTutorial?.tutorialCompletionRateAvg}
+                    precision={2}
+                    valueStyle={
+                      summaryStatisticsTutorial?.tutorialCompletionRateAvg < 50
+                        ? { color: "#cf1322" }
+                        : { color: "#3f8600 " }
+                    }
+                    prefix={
+                      summaryStatisticsTutorial?.tutorialCompletionRateAvg <
+                      50 ? (
+                        <ArrowDownOutlined />
+                      ) : (
+                        <ArrowUpOutlined />
+                      )
+                    }
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card bordered={true}>
+                  <Statistic
+                    title="Average Tutorial Score"
+                    value={
+                      summaryStatisticsTutorial?.tutorialScorePercentageAvg
+                    }
+                    precision={2}
+                    valueStyle={
+                      summaryStatisticsTutorial?.tutorialScorePercentageAvg < 50
+                        ? { color: "#cf1322" }
+                        : { color: "#3f8600" }
+                    }
+                    prefix={
+                      summaryStatisticsTutorial?.tutorialScorePercentageAvg <
+                      50 ? (
+                        <ArrowDownOutlined />
+                      ) : (
+                        <ArrowUpOutlined />
+                      )
+                    }
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+            </Row>
+          )}
+        </Card>
+        <Text>
+          <br />
           <b>Academic Performance & Learning Strategies</b>
         </Text>
-        {customMessage ? (
-          <Result
-            status={customMessage.type as ResultStatusType}
-            title={customMessage.content}
-          />
-        ) : (
-          <CustomStackedBarChart
-            chartData={academicPerformanceAndLearningStrategiesTutorial}
-          />
-        )}
+        : Average learner engagment with tutorial questions
+        <Card>
+          {customMessage ? (
+            <Result
+              status={customMessage.type as ResultStatusType}
+              title={customMessage.content}
+            />
+          ) : (
+            <CustomStackedBarChart
+              chartData={academicPerformanceAndLearningStrategiesTutorial}
+            />
+          )}
+        </Card>
+        <br />
+        <Text>
+          <b>Affective State</b>: Average feedback distribution for tutorial
+          questions
+        </Text>
+        <Card>
+          {customMessage ? (
+            <Result
+              status={customMessage.type as ResultStatusType}
+              title={customMessage.content}
+            />
+          ) : (
+            <CustomLabeledPieChart chartData={affectiveStateTutorial} />
+          )}
+        </Card>
       </div>
     </Fragment>
   );
--- frontend/src/utils/dashboard_analytics_util.ts
+++ frontend/src/utils/dashboard_analytics_util.ts
@@ -42,3 +42,77 @@ export const getAcademicPerformanceAndLearningStrategiesTutorial = (
       (unansweredShortAnswerQuestionCount / shortAnswerQuestions.length) * 100,
   };
 };
+
+export const getAffectiveStateTutorial = (results: any[]) => {
+  const totalFeedback = results.filter((item) => item.feedback_type !== null);
+  const skippedFeedback = totalFeedback.filter(
+    (item) => item.feedback_type === "skip"
+  );
+  const basicFeedback = totalFeedback.filter(
+    (item) => item.feedback_type === "basic"
+  );
+  const inDetailFeedback = totalFeedback.filter(
+    (item) => item.feedback_type === "in-detail"
+  );
+
+  return {
+    skippedFeedback: (skippedFeedback.length / totalFeedback.length) * 100,
+    basicFeedback: (basicFeedback.length / totalFeedback.length) * 100,
+    inDetailFeedback: (inDetailFeedback.length / totalFeedback.length) * 100,
+  };
+};
+
+export const getSummaryStatisticsTutorial = (results: any[]) => {
+  const totalTutorialCount = new Set(results.map((item) => item.tutorial_id));
+
+  // Group questions by tutorial_id
+  const tutorials = results.reduce((acc, item) => {
+    acc[item.tutorial_id] = acc[item.tutorial_id] || [];
+    acc[item.tutorial_id].push(item);
+    return acc;
+  }, {});
+
+  // Calculate completion percentage for each tutorial
+  const tutorialCompletionPercentages = Object.values(tutorials).map(
+    (tutorialQuestions: any) => {
+      const totalQuestions = tutorialQuestions.length;
+      const answeredQuestions = tutorialQuestions.filter(
+        (item: any) => item.student_answer !== null
+      ).length;
+      return (answeredQuestions / totalQuestions) * 100;
+    }
+  );
+
+  // Calculate score percentage for each tutorial
+  const tutorialScorePercentages = Object.values(tutorials).map(
+    (tutorialQuestions: any) => {
+      const totalQuestions = tutorialQuestions.length;
+      const correctQuestions = tutorialQuestions.filter(
+        (item: any) => item.is_student_answer_correct === true
+      ).length;
+      return (correctQuestions / totalQuestions) * 100;
+    }
+  );
+
+  // Calculate the average completion percentage across all tutorials
+  const totalPercentages = tutorialCompletionPercentages.reduce(
+    (sum, percentage) => sum + percentage,
+    0
+  );
+  const tutorialCompletionRateAvg =
+    totalPercentages / tutorialCompletionPercentages.length;
+
+  // Calculate the average tutorial score percentage across all tutorials
+  const totalScores = tutorialScorePercentages.reduce(
+    (sum, percentage) => sum + percentage,
+    0
+  );
+  const tutorialScorePercentageAvg =
+    totalScores / tutorialScorePercentages.length;
+
+  return {
+    totalTutorialCount: totalTutorialCount.size,
+    tutorialScorePercentageAvg,
+    tutorialCompletionRateAvg,
+  };
+};
