70032d10aaafb7e5b00fb314b0171ea65210260c
IT21001352 <it21001352@my.sliit.lk>
2024-10-25 05:21:41 +0530

    breadcrumbs updated

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -31,7 +31,6 @@ import LabOverview from "../pages/[module]/[lesson]/lab";
 import LabSession from "../pages/[module]/[lesson]/lab/[labID]";
 import Module from "../pages/[module]";
 import { Button, Result } from "antd";
-import Lesson from "../pages/[module]/[lesson]";
 import DiscussionForum from "../pages/[module]/[lesson]/discussionForum";
 import { DiscussionForumProvider } from "../provider/DiscussionForumContext";
 import { WebSocketProvider } from "../provider/WebSocketContext";
@@ -130,12 +129,24 @@ const Routes = () => {
           children: [
             {
               path: "",
-              element: <WebSocketProvider><DiscussionForumProvider><DiscussionForum /></DiscussionForumProvider></WebSocketProvider>
+              element: (
+                <WebSocketProvider>
+                  <DiscussionForumProvider>
+                    <DiscussionForum />
+                  </DiscussionForumProvider>
+                </WebSocketProvider>
+              ),
             },
             {
               path: ":discussionId",
-              element: <WebSocketProvider><DiscussionForumProvider><Discussion /></DiscussionForumProvider></WebSocketProvider>
-            }
+              element: (
+                <WebSocketProvider>
+                  <DiscussionForumProvider>
+                    <Discussion />
+                  </DiscussionForumProvider>
+                </WebSocketProvider>
+              ),
+            },
           ],
         },
         {
@@ -143,7 +154,7 @@ const Routes = () => {
           children: [
             {
               path: "",
-              element: <Lesson />,
+              element: <Navigate to="/database-systems" />,
             },
             {
               path: "tutorial",
